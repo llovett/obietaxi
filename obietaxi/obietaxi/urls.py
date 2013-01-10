6401c149da34b69530f2328b2dbe012ad975ed2a
Luke Lovett <llovett@oberlin.edu>
2013-01-10 17:44:16 -0500

    Added TODOs in views.py

--- obietaxi/obietaxi/urls.py
+++ obietaxi/obietaxi/urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.generic.simple import direct_to_template
+from taxi import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'obietaxi.views.home', name='home'),
-    # url(r'^obietaxi/', include('obietaxi.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       url( r'^$', direct_to_template, { 'template':'index.html' } ),
+                       url( r'^/trip/new/$', views.new_trip, name="trip_new" ),
+                       url( r'^/browse/$', views.list_trips, name="trip_list" ),
 )
--- obietaxi/taxi/views.py
+++ obietaxi/taxi/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def new_trip( request ):
     # TODO: take information from the request, and create a new Trip object.
@@ -8,4 +9,6 @@ def new_trip( request ):
 def list_trips( request ):
     # TODO: Pull all RideRequests from the database and render them in the
     # Browse template (must create the Browse template first)
-    return HttpResponse()
+    ride_requests = []
+    return render_to_response( "browse.html",
+                               { 'ride_requests': ride_requests } )
