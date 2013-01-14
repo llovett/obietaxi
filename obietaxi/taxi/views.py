b74d21f2bbb155dc7ee247d49df947ec67d8029a
peter-fogg <peter.fogg@oberlin.edu>
2013-01-14 11:57:34 -0500

    Filled in code for existing views, will do more later.

--- obietaxi/taxi/views.py
+++ obietaxi/taxi/views.py
@@ -1,14 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from models import RideRequest, Trip, User
 
 def new_trip( request ):
     # TODO: take information from the request, and create a new Trip object.
     # Save the Trip in the database, and redirect to Browse page
-    return HttpResponse()
+    trip = Trip(start=request.POST['start'], end=request.POST['end'],
+                driver=User.objects(id=request.POST['id']), passengers=[])
+    trip.save()
+    return HttpResponseRedirect('/browse')
 
 def list_trips( request ):
     # TODO: Pull all RideRequests from the database and render them in the
     # Browse template (must create the Browse template first)
-    ride_requests = []
+    ride_requests = RideRequests.objects
     return render_to_response( "browse.html",
                                { 'ride_requests': ride_requests } )
