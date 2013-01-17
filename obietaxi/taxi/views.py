5f951822dfe6feac1e6f1e8d2fecf656d962d742
Nathan Teetor <nteetor@oberlin.edu>
2013-01-17 16:44:42 -0500

    Updated browse.html and views.html. Small error when submitting information.

--- obietaxi/taxi/views.py
+++ obietaxi/taxi/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from models import RideRequest, Trip, UserProfile, RideOffer, Location
 from datetime import datetime
-import json
 from random import random
 from time import strptime,mktime
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+
 import sys
+import json
 
 def new_trip( request ):
     # TODO: take information from the request, and create a new Trip object.
@@ -87,40 +90,41 @@ def request_ride_new( request ):
     date = strptime("%s %s %s %s %s %s" % (I,M,p,Y,b,d),"%I %M %p %Y %b %d")
     date_stamp = datetime.fromtimestamp(mktime(date))
     
-    # if request.POST['user_role'] is None:
-    #     rr = RideRequest.objects.create( start=startLocation,
-    #                                      end=endLocation,
-    #                                      date=date_stamp
-    #                                      )
-
-    #     return redirect('request_show', role='passenger')
     if request.POST['user_role'] == 'driver':
         ro = RideOffer.objects.create( start=startLocation,
                                        end=endLocation,
                                        date=date_stamp
                                        )
-        
-        kwargs = {'role': 'driver'}
-        return redirect( 'request_show', **kwargs )
     else:
         rr = RideRequest.objects.create( start=startLocation,
                                          end=endLocation,
                                          date=date_stamp
                                          )
 
-        kwargs = {'role': 'driver'}
-        return redirect('request_show', **kwargs)
 
-def request_show( request, **kwargs ):
+    HttpResponseRedirect(reverse('request_show'))
+
+    if not request.POST['user_role']:
+        ride_requests = RideRequest.objects
+        ride_offers = RideOffer.objects
+    elif request.POST['user_role' == 'driver']:
+        ride_requests = RideRequest.objects
+    else:
+        ride_offers = RideOffer.objects
+        
+    return render_to_response("browse.html", locals(), context_instance=RequestContext(request))
+
+
+def request_show( request ):
     '''
     Lists all of the RideRequests and renders them to "browse.html"
     '''
     # TODO: Pull all RideRequests from the database and render them in the
     # "browse.html" template
 
-    if kwargs['role'] == 'driver':
-        ride_offers = RideOffer.objects
-        return render_to_response("browse.html", locals(), context_instance=RequestContext(request))
-    else:
-        ride_requests = RideRequest.objects
-        return render_to_response( "browse.html", locals(), context_instance=RequestContext(request) )
+    ride_requests = RideRequest.objects
+    ride_offers = RideOffer.objects
+
+    return render_to_response("browse.html", locals(), context_instance=RequestContext(request))
+
+    
