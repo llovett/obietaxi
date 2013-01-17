d7f06cae1e2adedb614a1e571d4a966b18acd558
Luke Lovett <llovett@oberlin.edu>
2013-01-17 17:11:05 -0500

    merged templates of mongoengine-login and taxi apps

--- obietaxi/mongoengine-login/urls.py
+++ obietaxi/mongoengine-login/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-                       url( r'^$', views.user_show, name="main_page" ),
                        url( r'^login/$', views.login_view, name="login" ),
                        url( r'^google/success/$', views.google_login_success, name="google_login_success" ),
                        url( r'^profile/$', views.user_show, name="login_success" ),
