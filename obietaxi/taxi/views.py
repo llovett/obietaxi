c4a729c17073cf9f59393b26d6d91a056a32cb3f
Luke Lovett <llovett@oberlin.edu>
2013-01-18 17:35:45 -0500

    started to make two different views for ride offers/requests

--- obietaxi/taxi/views.py
+++ obietaxi/taxi/views.py
@@ -61,67 +61,79 @@ def request_or_offer_ride( request ):
     form = RideRequestOfferForm()
     return render_to_response( 'index.html', locals(), context_instance=RequestContext(request) )
 
+def _make_request_or_offer( data, type ):
+    startloc = (float(data['start_lat']),float(data['start_lng']))
+    endloc = (float(data['end_lat']),float(data['end_lng']))
+    startLocation = Location( position=startloc, title=data['start_location'] )
+    endLocation = Location( position=endloc, title=data['end_location'] )
+    date = data['date']
+    kwargs = { 'start':startLocation, 'end':endLocation, 'date':date }
+
+    if type == 'offer':
+        return RideOffer.objects.create( **kwargs )
+    elif type == 'request':
+        return RideRequest.objects.create( **kwargs )
+
+def offer_ride_new( request ):
+    '''
+    Creates a new RideOffer from POST data given in <request>.
+
+    '''
+    requestForm = RideRequestOfferForm( request.POST )
+    if requestForm.is_valid():
+        data = requestForm.cleaned_data
+        rideRequest = _make_request_or_offer( data, 'offer' )
+        
+    ride_requests = RideRequest.objects
+    return render_to_response("browse.html", locals(), context_instance=RequestContext(request))    
+
 def request_ride_new( request ):
     '''
     Creates a new RideRequest from POST data given in <request>.
+
     '''
-    
-    end_lat = '0'
-    end_lon = '0'
-    start_lat = '0'
-    start_lon = '0'
-   
-    if request.POST['start_latitude'] != '':
-        start_lat = request.POST['start_latitude']
-        start_lon = request.POST['start_longitude']
-
-    if request.POST['end_latitude'] != '':
-        end_lat = request.POST['end_latitude']
-        end_lon = request.POST['end_longitude']
-
-    startloc = (float(start_lat),float(start_lon))
-    endloc = (float(end_lat),float(end_lon))
-    startLocation = Location( position=startloc, title=request.POST['start_point'] )
-    endLocation = Location( position=endloc, title=request.POST['end_point'] )
-
-    # save data for creating a time struct w/ strptime
-    I = request.POST['time_start_hour']
-    M = request.POST['time_start_minutes']
-    p = request.POST['time_start_pam']
-    Y = request.POST['date_year']
-    b = request.POST['date_month']
-    d = request.POST['date_day']
-    
-    date = strptime("%s %s %s %s %s %s" % (I,M,p,Y,b,d),"%I %M %p %Y %b %d")
-    date_stamp = datetime.fromtimestamp(mktime(date))
-    
-    if request.POST['user_role'] == 'driver':
-        ro = RideOffer.objects.create( start=startLocation,
-                                       end=endLocation,
-                                       date=date_stamp
-                                       )
-    else:
-        rr = RideRequest.objects.create( start=startLocation,
-                                         end=endLocation,
-                                         date=date_stamp
-                                         )
-
-
-    HttpResponseRedirect(reverse('request_show'))
-
-    if not request.POST['user_role']:
-        ride_requests = RideRequest.objects
-        ride_offers = RideOffer.objects
-    elif request.POST['user_role' == 'driver']:
-        ride_requests = RideRequest.objects
-    else:
-        ride_offers = RideOffer.objects
+    requestForm = RideRequestOfferForm( request.POST )
+    if requestForm.is_valid():
+        data = requestForm.cleaned_data
+        rideRequest = _make_request_or_offer( data, 'request' )
         
-    return render_to_response("browse.html", locals(), context_instance=RequestContext(request))
+    ride_offers = RideOffer.objects
+    return render_to_response("browse.html", locals(), context_instance=RequestContext(request))    
+        
+    # # save data for creating a time struct w/ strptime
+    # I = request.POST['time_start_hour']
+    # M = request.POST['time_start_minutes']
+    # p = request.POST['time_start_pam']
+    # Y = request.POST['date_year']
+    # b = request.POST['date_month']
+    # d = request.POST['date_day']
+    
+    # date = strptime("%s %s %s %s %s %s" % (I,M,p,Y,b,d),"%I %M %p %Y %b %d")
+    # date_stamp = datetime.fromtimestamp(mktime(date))
+    
+    # if request.POST['user_role'] == 'driver':
+    #     ro = RideOffer.objects.create( start=startLocation,
+    #                                    end=endLocation,
+    #                                    date=date_stamp
+    #                                    )
+    # else:
+    #     rr = RideRequest.objects.create( start=startLocation,
+    #                                      end=endLocation,
+    #                                      date=date_stamp
+    #                                      )
 
 
-def offer_ride_new( request ):
-    return HttpResponse("offering a new ride, eh? Just you wait until we write this code!")
+    # HttpResponseRedirect(reverse('request_show'))
+
+    # if not request.POST['user_role']:
+    #     ride_requests = RideRequest.objects
+    #     ride_offers = RideOffer.objects
+    # elif request.POST['user_role' == 'driver']:
+    #     ride_requests = RideRequest.objects
+    # else:
+    #     ride_offers = RideOffer.objects
+        
+    # return render_to_response("browse.html", locals(), context_instance=RequestContext(request))
 
 def request_show( request ):
     '''
