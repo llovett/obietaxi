f3dc4ae40373a04e5b569ead150ffab368fc2e49
Luke Lovett <llovett@oberlin.edu>
2013-02-11 21:10:31 -0500

    added some things to browse-elements.js

--- obietaxi/taxi/static/js/browse-elements.js
+++ obietaxi/taxi/static/js/browse-elements.js
@@ -10,8 +10,23 @@ $(document).ready(
 	// Next button to take user to map for offering a ride
 	$("#offer_show_map_button").click(
 	    function() {
-		// TODO: show the map!
-		console.log("show the map");
+		$("#new_offer_modal_form").hide();
+		$("#new_offer_modal_map").show();
+	    }
+	);
+
+	// Back button to take user back to form for offering a ride
+	$("#offer_show_form_button").click(
+	    function() {
+		$("#new_offer_modal_map").hide();
+		$("#new_offer_modal_form").show();
+	    }
+	);
+
+	// Submit button when offering a ride
+	$("#offer_ride_button").click(
+	    function() {
+		$("#offer_form").submit();
 	    }
 	);
     }
