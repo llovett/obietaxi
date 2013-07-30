c2bc8c63526d962166eafd498cb00ede44558494
Luke Lovett <lukeharmonlovett@gmail.com>
2013-07-30 12:58:45 -0700

    beginning to add tests

--- obietaxi/obietaxi/settings.py
+++ obietaxi/obietaxi/settings.py
@@ -136,6 +136,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+TEST_RUNNER = 'mongorunner.TestRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
--- obietaxi/taxi/tests.py
+++ obietaxi/taxi/tests.py
@@ -5,12 +5,26 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from mongorunner import TestCase
+from django.test.client import RequestFactory, Client
+from taxi import views
+from taxi import models
+from datetime import datetime
 
+class BrowseTest(TestCase):
+    def _fixture_setup(self):
+        pass
+    def _fixture_teardown(self):
+        pass
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+
+    def test_all_listings(self):
+        ride_requests = list(models.RideRequest.objects.filter( date__gte=datetime.now(), ride_offer=None ))
+        ride_offers = list(models.RideOffer.objects.filter( date__gte=datetime.now() ))
+        all_listings = set(ride_requests + ride_offers)
+        response = self.client.get("/browse/")
+        test_listings = set(list(response.context["ride_requests"]) + list(response.context["ride_requests"]))
+        self.assertEqual(test_listings, all_listings)
